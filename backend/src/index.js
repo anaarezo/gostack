e3d87b6955f30258bef3528fccfc598880ea428c
anaarezo <laura.arezo@gmail.com>
2020-08-19 17:50:11 -0300

    NodeJS: Aplicação Funcional

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { uuid } = require('uuidv4');
 const app = express();
 
 app.use(express.json());
@@ -22,52 +22,61 @@ app.use(express.json());
   * Request body: conteúdo na hora de criar e editar um recurso
   */
 
-app.get('/projects', (request, response) => {
-  //const query = request.query; abaixo como desestruturar parâmetros
-  //console.log(query);
-  const { title, owner } = request.query;
-  console.log(title);
-  console.log(owner);
+const projects = [];
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+app.get('/projects', (request, response) => {
+  const { title } = request.query;
+
+  const results = title
+  ? projects.filter(project => project.title.includes(title))
+  : projects;
+  return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
-  // const body = request.body;
-  // console.log(body);
   const { title, owner } = request.body;
-  console.log(title);
-  console.log(owner);
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
 });
 
 // PUT atualizar 1 projeto e não todos os projetos usando identificador único e.g. http://localhost:3333/projects/2
 app.put('/projects/:id', (request, response) => {
-  //const params = request.params;
-  //console.log(params);
-
   const { id } = request.params;
-  console.log(id);
 
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const { title, owner } = request.body;
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if(projectIndex < 0) {
+    return response.status(400).json({error: 'Project not found.'});
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if(projectIndex < 0) {
+    return response.status(400).json({error: 'Project not found.'});
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
